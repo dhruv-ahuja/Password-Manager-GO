8931226a03fde8028714e7945abc4e4d88f247e7
Dhruv <dhruvahuja2k@gmail.com>
2021-10-27 23:16:59 +0530

    shifted tableExists to connect.go

--- database/connect.go
+++ database/connect.go
@@ -8,25 +8,9 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// Make table if it doesn't yet exist
-func MakeTable(db *sql.DB) error {
-	// Get the path to the SQL file, using "/database" since I guess filepath is relative and we are executing the program from main
-	path := "./database/setup.sql"
-	// Read the file content
-	queries, err := os.ReadFile(path)
-	if err != nil {
-		return err
-	}
-
-	// Convert queries to a string since ReadFile returns a slice of bytes but db.Query only accepts string
-	if _, tableErr := db.Exec(string(queries)); tableErr != nil {
-		return tableErr
-	} else {
-		return nil
-	}
-}
-
+// Connect to Database
 func ConnectToDB(host string, port int, username, password, dbname string) (*sql.DB, error) {
+
 	// Prepare postgres connection parameters
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, username, password, dbname)
 
@@ -44,4 +28,44 @@ func ConnectToDB(host string, port int, username, password, dbname string) (*sql
 	fmt.Println("Connected to DB successfully!")
 	return db, nil
 
+}
+
+// Check whether the table to use in further operations has already been created
+func TableExists(db *sql.DB, table string) error {
+
+	// SQL query to run
+	getTable := fmt.Sprintf("select * from %s", table)
+
+	if _, tableErr := db.Query(getTable); tableErr != nil {
+
+		// if there's no table, means the user is using the application for the first time
+		fmt.Println("First-time execution; creating table...")
+
+		// Call the MakeTable function
+		if makeTableErr := MakeTable(db); makeTableErr != nil {
+			return makeTableErr
+		}
+		// Let the user know that the table has been created
+		fmt.Println("Everything done. You're good to go.")
+	}
+	return nil
+}
+
+// Make table if it doesn't yet exist
+func MakeTable(db *sql.DB) error {
+
+	// Get the path to the SQL file
+	path := "./database/setup.sql"
+	// Read the file content
+	queries, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	// Convert queries to a string since ReadFile returns a slice of bytes but db.Query only accepts string
+	if _, tableErr := db.Exec(string(queries)); tableErr != nil {
+		return tableErr
+	} else {
+		return nil
+	}
 }
