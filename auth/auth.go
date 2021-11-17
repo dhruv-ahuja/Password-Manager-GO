c6be1ea2527a04f1f2c92b4fba18a19282bf4ac5
Dhruv <dhruvahuja2k@gmail.com>
2021-11-17 21:05:21 +0530

    Started auth/auth.go revamp - Added LoadEncryptedInfo function that reads saved encrypted data which is needed when saving passwords - Changed AuthUser function to now use argon2

--- auth/auth.go
+++ auth/auth.go
@@ -1,12 +1,14 @@
 package auth
 
 import (
+	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
 	"os"
 
 	"github.com/good-times-ahead/password-manager-go/app"
-	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/argon2"
 )
 
 // Check whether master password file exists already
@@ -29,25 +31,63 @@ func CheckMasterPassword(pwFilePath string) bool {
 
 }
 
-// Take master password hash and compare it to the stored hash, allowing or disallowing them access to the application
-func AuthorizeUser(pwFilePath string) error {
-	// Load the hash from the file
+func LoadEncryptedInfo(encInfoPath string) ([][]byte, error) {
+	// 1. salt, 2. sealedkey, 3. nonce
+	file, err := os.Open(encInfoPath)
+
+	if err != nil {
+		return nil, errors.New("error reading from encrypted data file, please check")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	count := 0
+
+	var salt, sealedKey, nonce []byte
+
+	for scanner.Scan() {
+
+		count += 1
+
+		switch count {
+
+		case 1:
+			salt = []byte(scanner.Text())
+		case 2:
+			sealedKey = []byte(scanner.Text())
+		case 3:
+			nonce = []byte(scanner.Text())
+		default:
+			break
+
+		}
+
+	}
+
+	values := [][]byte{salt, sealedKey, nonce}
+
+	return values, nil
+}
+
+// Take users' master password input and compare it to the stored hash, allowing access if match
+func AuthorizeUser(pwFilePath string, values [][]byte) error {
+	// Load master password file
 	hashedPassword, err := os.ReadFile(pwFilePath)
 
 	if err != nil {
-		return errors.New("error encountered when attempting to read from the password file")
+		return errors.New("error when trying to read master password file, please check")
 	}
 
-	// Infinite loop till the user correctly enters the password
 	for true {
 		// Take user input
 		prompt := "Enter the Master Password: "
 		usrInput := app.GetInput(prompt)
 
-		// Compare hash and password, returns nil if match else error
-		compare := bcrypt.CompareHashAndPassword(hashedPassword, []byte(usrInput))
+		compare := argon2.IDKey([]byte(usrInput), values[0], 1, 64*1024, 4, 32)
 
-		if compare == nil {
+		if bytes.Equal(compare, hashedPassword) {
 			return nil
 		}
 
@@ -55,7 +95,5 @@ func AuthorizeUser(pwFilePath string) error {
 		fmt.Println()
 
 	}
-
 	return nil
-
 }
