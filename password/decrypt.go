4f4ec9ece6fdad403c2b3bb0d70f30a2251e9f81
Dhruv <dhruvahuja2k@gmail.com>
2021-11-26 14:16:56 +0530

    Split password.go - Split password/password.go into 2 distinct files, "encrypt.go" & "decrypt.go" to further clean up directory.

--- password/decrypt.go
+++ password/decrypt.go
@@ -3,47 +3,12 @@ package password
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/rand"
 	"encoding/base64"
 	"errors"
 	"fmt"
-	"io"
 
 	"github.com/good-times-ahead/password-manager-go/credentials"
 )
-
-// Encrypt the plain-text password
-func Encrypt(encryptionKey []byte, c credentials.Credentials) (string, error) {
-
-	password := []byte(c.Password)
-
-	generatedCipher, err := aes.NewCipher(encryptionKey)
-
-	if err != nil {
-		return "", errors.New("error generating cipher")
-	}
-
-	// GCM is a mode of operation used on block ciphers
-	gcm, err := cipher.NewGCM(generatedCipher)
-
-	if err != nil {
-		return "", errors.New("error generating GCM")
-	}
-
-	nonce := make([]byte, gcm.NonceSize())
-
-	//populates our byte array with a cryptographically secure sequence
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		return "", errors.New("error generating secured-sequence")
-	}
-
-	encryptedPassword := gcm.Seal(nonce, nonce, password, nil)
-
-	// convert the slice of encrypted bytes into a base64 encrypted string to store in the database
-	b64Password := base64.StdEncoding.EncodeToString(encryptedPassword)
-
-	return b64Password, nil
-}
 
 // Decrypt the base64 encoded password
 func Decrypt(base64Password string, encryptionKey []byte, c credentials.Credentials) (string, error) {
---
+++ password/encrypt.go
@@ -0,0 +1,45 @@
+package password
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+
+	"github.com/good-times-ahead/password-manager-go/credentials"
+)
+
+// Encrypt the plain-text password
+func Encrypt(encryptionKey []byte, c credentials.Credentials) (string, error) {
+
+	password := []byte(c.Password)
+
+	generatedCipher, err := aes.NewCipher(encryptionKey)
+
+	if err != nil {
+		return "", errors.New("error generating cipher")
+	}
+
+	// GCM is a mode of operation used on block ciphers
+	gcm, err := cipher.NewGCM(generatedCipher)
+
+	if err != nil {
+		return "", errors.New("error generating GCM")
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+
+	//populates our byte array with a cryptographically secure sequence
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", errors.New("error generating secured-sequence")
+	}
+
+	encryptedPassword := gcm.Seal(nonce, nonce, password, nil)
+
+	// convert the slice of encrypted bytes into a base64 encrypted string to store in the database
+	b64Password := base64.StdEncoding.EncodeToString(encryptedPassword)
+
+	return b64Password, nil
+}
